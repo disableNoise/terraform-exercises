3670d1ee784d1f8b5977f65fd6b44168dd25e1ed
disableNoise <recargaxsocial@protonmail.com>
2025-08-18 14:45:20 -0400

    added level 1

---
+++ level1/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "Learning_Terraform_celgueda"
+
+    workspaces {
+      project = "Learn Terraform Level 1"
+      name = "learn-terraform-level-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
